ff7fe2cdbcc81eba12b523a1e4469cd0005f86b7
Krishan Kumar Safi <99599853+Krishukr12@users.noreply.github.com>
2022-05-17 14:03:08 +0530

    some changes

--- react-class/src/App.js
+++ react-class/src/App.js
@@ -1,12 +1,10 @@
 
 import './App.css';
-import MobileOs from './sprint-1/day-1/MobileOs';
-import { Mobilemanifature } from './sprint-1/day-1/Mobilemanifature';
+import { Navbar } from './sprint-1/day-1/navbar/Navbar';
 function App() {
   return (
     <div className="App">
-      <MobileOs/>
-      <Mobilemanifature/>
+  <Navbar/>
     </div>
   );
 }
---
+++ react-class/src/sprint-1/day-1/navbar/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import classes from "./navbar.module.css"
+export const Navbar = () => {
+  return (
+    <div className={classes.navBar}>
+        <div className={classes.navBar2}></div>
+    </div>
+  )
+}
