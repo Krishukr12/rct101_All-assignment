ba05de3081065113668bd6f0643bf179e237ec6c
Krishan Kumar Safi <99599853+Krishukr12@users.noreply.github.com>
2022-05-18 17:10:59 +0530

    some css added

--- react-class/src/App.js
+++ react-class/src/App.js
@@ -1,9 +1,8 @@
 import "./App.css";
-import Counter from "./sprint-1/day-2/Assignment/Counter"
 function App() {
   return (
     <div className="App">
-     <Counter intialvalue={0}/>
+
     </div>
   );
 }
