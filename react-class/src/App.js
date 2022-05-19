8822a4300143f94b957d2687d5327a04a95762b5
Krishan Kumar Safi <99599853+Krishukr12@users.noreply.github.com>
2022-05-19 07:40:25 +0530

    some Css changes

--- react-class/src/App.js
+++ react-class/src/App.js
@@ -1,8 +1,9 @@
 import "./App.css";
+import {Payment} from "./sprint-1/day-3/Assignment/Payment Card/Payment"
 function App() {
   return (
     <div className="App">
-
+       <Payment/>
     </div>
   );
 }
--- react-class/src/sprint-1/day-3/Assignment/Payment Card/Payment.jsx
+++ react-class/src/sprint-1/day-3/Assignment/Payment Card/Payment.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
-
+import styled from "styled-components"
 export const Payment = () => {
+  const Div=styled.div`
+  height:390px;
+  width:700px;
+  // border:1px solid red;
+  background-color:rgb(255,165,0);
+  `
   return (
-    <div>Payment</div>
+    <Div>Payment</Div>
   )
 }
