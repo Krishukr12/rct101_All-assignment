0a79aec67c65d0515292f6a7c3cefcd98018b543
Krishan Kumar Safi <99599853+Krishukr12@users.noreply.github.com>
2022-05-21 18:08:45 +0530

    some css added

--- react-class/src/sprint-1/day-3/I/We/Todo.jsx
+++ react-class/src/sprint-1/day-3/I/We/Todo.jsx
@@ -6,6 +6,7 @@ export const Todo = () => {
   const [todos, setTodos] = useState([]);
   const addTodos = (newTodos) => {
     setTodos([...todos, newTodos]);
+    console.log(todos.length);
   };
   return (
     <div>
