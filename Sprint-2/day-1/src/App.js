b68675baee531230c047332ec806cd50bd9a66bf
Krishan Kumar Safi <99599853+Krishukr12@users.noreply.github.com>
2022-05-25 19:38:47 +0530

    css modification

--- Sprint-2/day-1/src/App.js
+++ Sprint-2/day-1/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Be ready For Today's challange</h1>
+      <h1>Krishan</h1>
     </div>
   );
 }
